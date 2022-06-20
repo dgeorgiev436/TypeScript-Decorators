0c1886740e2f65ba52806b5bd7c22c7789cd9934
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-06-20 00:01:00 +0000

    More decorators

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,28 @@
 // Decorator
 function Logger(logString: string) {
 	return function(constructor: Function){
-		console.log("This is " + this.logString);
+		console.log(logString);
 		console.log(constructor);	
+	}
+}
+
+// Decorators are developed to be used by other developers to provide extra utilities
+function WithTemplate(template: string, hookId: string){
+	return function(constructor: any){
+		const hookElement = document.getElementById(hookId);
+		const p = new constructor()
+		if(hookElement){
+			hookElement.innerHTML = template;
+			hookElement.querySelector("h1")!.textContent = p.name;
+			
+		}
 	}
 }
 
 // Applying Decorator with @ to class
 // Decorator gets executed when class is defined
-@Logger("Logging - Person")
+// @Logger("Logging - Person")
+@WithTemplate("<h1>My Person Object </h1>", "app")
 class Person {
 	name = "Max";
 	
