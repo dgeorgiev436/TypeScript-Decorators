5b2191319fb1e172ebb21d5ce0a1257ccf1c8a0b
dgeorgiev436 <dgeorgiev436@gmail.com>
2022-06-26 00:23:14 +0000

    Using decorators for validationn

--- src/app.ts
+++ src/app.ts
@@ -128,3 +128,28 @@ const printer1 = new Printer();
 
 const button = document.querySelector("button")!;
 button.addEventListener("click", printer1.showMessage);
+
+
+class Course {
+	title: string;
+	price: number;
+	
+	constructor(t: string, p: number){
+		this.title = t;
+		this.price = p;
+	}
+}
+
+const courseForm = document.querySelector("form")!;
+courseForm.addEventListener("submit", event => {
+	event.preventDefault();
+	const titleEl = document.getElementById("title") as HTMLInputElement;
+	const priceEl = document.getElementById("price") as HTMLInputElement;
+	
+	const title = titleEl.value;
+	const price = +priceEl.value;
+	
+	
+	const createdCourse = new Course(title, price);
+	console.log(createdCourse);
+})
